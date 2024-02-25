7f3381b0acfce2c28003122dd696af5040f0d4ea
Elena <elebegh@outlook.com>
2024-02-25 18:24:45 +0100

    fatto animazioni Netflix clone

--- M3/Week4/D1 - Netflix/script.js
+++ M3/Week4/D1 - Netflix/script.js
@@ -2,7 +2,7 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: false,
-    slidesPerView: 5,
+    slidesPerView: 'auto',
     spaceBetween: 20,
   
     // If we need pagination
@@ -21,4 +21,43 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-scrollbar',
     },
   });
+  
+  const watchCarousel = (carousels) => {
+    carousels.forEach(carousel => {
+      // if (carousel.isIntersecting) {
+      //   console.log(carousel)
+      //   carousel.target.classList.add("container-enter-animation");
+      //   carousel.target.classList.remove("container-hidden");
+      // }
+      // else {
+      //   console.log("elemento è uscito dalla viewport");
+      //   carousel.target.classList.remove("container-enter-animation");
+      //   carousel.target.classList.add("container-hidden");
+      // }
+      if (carousel.boundingClientRect.top > 0 && carousel.isIntersecting) {
+        carousel.target.classList.add("container-enter-animation");
+        carousel.target.classList.remove("container-hidden");
+      } else if (carousel.boundingClientRect.top < 0 && carousel.isIntersecting) {
+        carousel.target.classList.add("container-exit-animation");
+        carousel.target.classList.remove("container-hidden");
+      } else {
+        carousel.target.classList.add("container-hidden");
+        carousel.target.classList.remove("container-enter-animation");
+        carousel.target.classList.remove("container-exit-animation");
+      }
+    })
+  }
+
+  const carousels = document.querySelectorAll(".movie-carousel");
+  const observer = new IntersectionObserver(watchCarousel, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.3, 
+  })
+
+  carousels.forEach(carousel => {
+    observer.observe(carousel)
+  })
+
+
   
