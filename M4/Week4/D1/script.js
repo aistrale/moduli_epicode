d9816883899a1462c6875d4dc25b6d0a83a60ffb
Elena <elebegh@outlook.com>
2024-03-26 19:56:42 +0100

    cominciato compito finale W4

---
+++ M4/Week4/D1/script.js
@@ -0,0 +1,18 @@
+/* const ENDPOINT = "https://striveschool-api.herokuapp.com/api/product/";
+const KEY = "Bearer";
+const OPTIONS = {
+    method: 
+    headers: {
+        "Authorization": KEY,
+        "Content-Type": "application/json",
+    }
+}
+
+const getProduct = async () => {
+    try {
+        const response = await fetch(ENDPOINT) {
+        }
+    } catch (error) {
+        
+    }
+} */
