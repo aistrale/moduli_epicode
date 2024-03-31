7055b2b29b1d3af0d91697ae38e58c47544020a9
Elena <elebegh@outlook.com>
2024-03-31 23:45:18 +0200

    aggiunto lezione di live coding

---
+++ M4/Week4/Live coding - agenda Epicode/js/script.js
@@ -0,0 +1,79 @@
+// In questo script andremo a implementare il CRUD sull'endpoint dell'Agenda di EPICODE.
+const ENDPOINT = "https://striveschool-api.herokuapp.com/api/agenda"
+
+async function getAgenda() {
+    const tableBody = document.querySelector("tbody")
+    try {
+        tableBody.innerHTML = /*html*/
+            `<tr>
+                <td colspan="6">
+                    <div class="d-flex my-5">
+                        <div class="spinner-border mx-auto" role="status">
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                    </div>
+                </td>
+            </tr>`
+        const response = await fetch(ENDPOINT) // la variabile response crea una variabile "data"
+        const data = await response.json() // la variabile "data" è a sua volta una promessa con i risultati
+        return data // questa è la risoluzione della promessa
+    } catch (error) {
+        console.log('here')
+        console.error(error)
+        tableBody.innerHTML = error.message
+    }
+}
+
+function displayAgenda(entries) {
+    const tableBody = document.querySelector("tbody")
+    tableBody.innerHTML = entries.map((entry, i) => /*html*/`
+        <tr>
+            <th scope="row">${i + 1}</th>
+            <td>${entry.name}</td>
+            <td>${entry.description}</td>
+            <td>${entry.price}</td>
+            <td>${entry.time}</td>
+            <td>
+                <a href="/event.html?id=${entry._id}" class="btn btn-outline-secondary">
+                    <i class="bi bi-pencil-square"></i>
+                </a>
+                <button class="btn btn-outline-danger" onclick="handleDelete('${entry._id}')">
+                    <i class="bi bi-trash"></i>
+                </button>
+            </td>
+        </tr>
+    `).join("")
+}
+
+async function handleDelete(id) {
+
+    // Ci assicuriamo che l'utente confermi la cancellazione
+    // È normale per le operazioni distruttive assicurarsi di avere informato l'utente
+    if (!confirm("Sei sicuro di voler cancellare questo evento?")) return
+
+    const response = await fetch(ENDPOINT + '/' + id, {
+        method: "DELETE"
+    })
+
+    if (response.ok) {
+        alert("Evento cancellato!")
+        displayAgenda(await getAgenda())
+    }
+}
+
+async function handleFilter(filterString) {
+    const filterType = document.querySelector('select').value // "name" or "description"
+
+    // recupero i dati...
+    const events = await getAgenda()
+
+    displayAgenda(events.filter(
+        event => event[filterType].toLowerCase().includes(filterString.toLowerCase())
+        // event => event["name"].includes(filterString)
+        // event => event["description"].includes(filterString)
+    ))
+}
+
+window.onload = async () => {
+    displayAgenda(await getAgenda())
+}
