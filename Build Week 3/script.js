9a4f5e2be1f751261d3968759f18a5ff6346631d
Elena <elebegh@outlook.com>
2024-04-05 20:52:13 +0200

    BW3

--- Build Week 3/script.js
+++ Build Week 3/script.js
@@ -1,4 +1,5 @@
 const ENDPOINT = "https://striveschool-api.herokuapp.com/api/deezer/search?q=";
+let lastScrollPosisition = 0;
 
 const search = async () => {
     const inputValue = document.querySelector("#searchField").value;
@@ -6,8 +7,24 @@ const search = async () => {
      
     try {
         const response = await fetch(ENDPOINT + inputValue);
-        return response.json();
+        const data = await response.json();
+        console.log(data)
+        const artistID = data.data[0].artist.id;
+
+        window.location.href = `./artist.html?artistid=${artistID}`;
+
     } catch (error) {
         console.log(error);
     }
 }
+
+window.addEventListener("scroll", () => {
+    let currentScrollPosition = window.scrollY;
+    const div = document.querySelector("#fixedDiv")
+    if(currentScrollPosition > lastScrollPosisition && currentScrollPosition > 100) {
+        div.classList.add("transition");
+    } else {
+        div.classList.remove("transition");
+    } 
+    console.log(currentScrollPosition)
+})
